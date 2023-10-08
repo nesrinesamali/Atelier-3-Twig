eeca63467b46ee9ec00280683a70cd8c1ba546a3
Tayssirsboui <tayssirsboui@gmail.com>
2023-10-08 23:47:26 +0100

    Atelier4-p1

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -1,10 +1,21 @@
 <?php
 
 namespace App\Controller;
-
+use App\Form\AuthorType;
+use App\Repository\AuthorRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Author;
+
+
+
+
+
 
 class AuthorController extends AbstractController
 {
@@ -39,5 +50,106 @@ class AuthorController extends AbstractController
     {
         return $this->render('author/showAuthor.html.twig', ['authors'=>$id]);
     }
+
+
+    #[Route('/listAuthor', name:'list_author')]
+    public function listAuthor(AuthorRepository $authorrepository): Response
+    {
+        $list=$authorrepository->findAll();
+        return $this->render('author/listAuthor.html.twig', ['authors'=>$list]);
+    }
+
+    
+
+
+    #[Route('/AjoutStatique', name: 'author_ajoutStatique')]
+    public function ajoutStatique(EntityManagerInterface $entityManager): Response
+    {
+        
+        $author1 = new Author();
+        $author1->setUsername("Molière");
+        $author1->setEmail("Molière@gmail.com"); 
+
+        $entityManager->persist($author1);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('list_author');
+    }
+    #[Route('/Ajout', name: 'author_ajout')]
+
+    public function  Ajout (Request  $request)
+    {
+        $author=new Author();
+        $form =$this->CreateForm(AuthorType::class,$author);
+        $form->add('Save',SubmitType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($author);
+            $em->flush();
+            return $this->redirectToRoute('list_author');
+        }
+        return $this->render('author/ajout.html.twig',['form'=>$form->createView()]);
+    
+    }
+    #[Route('/edit/{id}', name: 'author_edit')]
+    public function modifier(AuthorRepository $repository, $id, Request $request)
+    {
+        $author = $repository->find($id);
+        $form = $this->createForm(AuthorType::class, $author);
+        $form->add('Edit', SubmitType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush(); 
+            return $this->redirectToRoute("list_author");
+        }
+
+        return $this->render('author/modifier.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    // #[Route('/delete/{id}', name: 'author_delete')]
+    // public function deleteAuthor(Request $request,$id, ManagerRegistry $manager): Response
+    // {
+    //     $em = $manager->getManager();
+    //     $authorrepository = $em->getRepository(Author::class);
+    //     $author = $authorrepository->find($id);
+
+    //     $em->remove($author);
+    //     $em->flush();
+    //     $list = $authorrepository->findAll();
+    //     return $this->render('author/listAuthor.html.twig', ['authors'=>$list]);
+    // }
+    #[Route('/delete/{id}', name: 'author_delete')]
+public function deleteAuthor(Request $request, $id, ManagerRegistry $manager): Response
+{
+    $em = $manager->getManager();
+    $authorRepository = $em->getRepository(Author::class);
+
+    // Recherche de l'auteur par son identifiant
+    $author = $authorRepository->find($id);
+
+    // Vérification si l'auteur a été trouvé
+    if ($author !== null) {
+        // Suppression de l'auteur
+        $em->remove($author);
+        $em->flush();
+
+        // Récupération de la liste mise à jour des auteurs après la suppression
+        $list = $authorRepository->findAll();
+
+        // Rendu de la vue avec la liste mise à jour des auteurs
+        return $this->render('author/listAuthor.html.twig', ['authors' => $list]);
+    } else {
+        // Gestion du cas où l'auteur n'a pas été trouvé
+        return new Response('Auteur non trouvé', Response::HTTP_NOT_FOUND);
+    }
+}
+
+
 }
 
